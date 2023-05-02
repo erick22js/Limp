a86b6e8e93194ba164f0e282d9e5de98e4944a6d
Erick22 <52467300+erick22js@users.noreply.github.com>
2023-05-02 11:16:33 -0300

    Delete test/luis directory

--- test/luis/sysutil.asm
+++
@@ -1,8 +0,0 @@
-.once
-
-/* Wait until a port sends a input value */
-.macro waitPort port:Reg
-	_: inus port
-	jr.co @_ ; Loops until port did not send a return message
-	clo ; Cleans the odd flag
-.endmacro
